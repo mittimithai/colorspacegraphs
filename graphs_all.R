dc818fa3c464983d12f0e4914f38be730f8b4c89
GIT Admin <maneesh.yadav@sri.com>
2016-11-10 17:55:06 -0800

    Fixed transformation matrix so that xy diagram looks correct and wavelengths map to right colors. Made graphs more consistent. Temporarily removed "pigment" gamut. Added R,G,B to the FORGA39 to inks to make hexagon.  Please enter the commit message for your changes. Lines starting

--- graphs_all.R
+++ graphs_all.R
@@ -7,14 +7,14 @@ library(scatterplot3d)
 library(rgl)
 library(geometry)
 
-o.df<-StockmanMacLeodJohnson2degCIEadjConeFundamentals1993
-
-colnames(o.df)=c("wavelength","L","M","S")
-o.df[,c('L','M','S')]<-10^(o.df[,c('L','M','S')])
+#careful using datasets from colorscience packages, some are log some are linear and they aren't labelled in a way that makes it easy to tell...they should all be linear
+o.df<-StockmanSharpe2degCMFadj2000
 o.df
 
-purple1.wl<-420
-purple2.wl<-630
+colnames(o.df)=c("wavelength","L","M","S")
+
+purple1.wl<-400
+purple2.wl<-615
 lms.df <- o.df %>% filter(wavelength>=purple1.wl,wavelength<=purple2.wl )
 lms.df
 
@@ -31,16 +31,11 @@ lmss<-as.list(data.frame(apply(lms.df[,c("L","M","S")],1,c)))
 names(lmss)<-lms.df$wavelength
 lmss
 
-#von Kries from wikipedia https://en.wikipedia.org/wiki/LMS_color_space
-#lmst<-solve(t(matrix(c( 0.38971, 0.68898, -0.07868,
-#                       -0.22981, 1.18340, 0.04641,
-#                        0,       0,        1), 3,3)))
 
-#D65 norm'd
-lmst<-solve(t(matrix(c( 0.4002, 0.7076,   -0.0808,
-                       -0.2263, 1.1653,    0.0457,
-                        0,       0,        0.9182), 3,3)))
-
+#http://www.cvrl.org/database/text/cienewxyz/cie2012xyz2.htm
+lmst<-t(matrix(c( 1.94735469,  -1.41445123,   0.36476327,
+                  0.68990272,   0.34832189,            0,
+                           0,            0,   1.93485343), 3,3))
                               
 
 #https://en.wikipedia.org/wiki/SRGB
@@ -85,8 +80,6 @@ spectrum.df$wl.labels[as.logical(spectrum.df$wavelength < 400 | spectrum.df$wave
 
 spectrum.df
 
-
-
 #this section needs to be run to translate rgl graphs with mouse, need to run this after you create the rgl window
 pan3d <- function(button) {
 start <- list()
@@ -111,18 +104,23 @@ cat("Callbacks set on button", button, "of rgl device",rgl.cur(),"\n")
 }
 pan3d(2)
 
-
+#for grabbing rgl view matrix
+zoom<-par3d()$zoom
+userMatrix<-par3d()$userMatrix
+windowRect<-par3d()$windowRect
+zoom
+paste(userMatrix,collapse=",")
+windowRect
 
 #cone spectral sensitivities
-t.df <- o.df %>% gather(cone.type, response, L:S)
+t.df <- spectrum.df %>% gather(cone.type, response, L:S)
 
-ggplot(t.df, aes(x=wavelength,y=response,group=cone.type))+
-stat_wl_strip(alpha = 1, ymin = -Inf, ymax = Inf)+
-scale_fill_identity()+
-geom_line(color="white",size=2)+
+ggplot(t.df, aes(x=wavelength,y=response,group=cone.type,color=rgb(R,G,B)))+
+geom_point(size=2)+
+geom_line()+
+scale_color_identity()+
 geom_text(data=data.frame(cone.type=c('L','M','S'),wavelength=c(605,505,460),response=c(0.8,0.8,0.8)), aes(label=cone.type),size=8,hjust=0,vjust=-1,color="white")+
 labs(x="wavelength (nm)", y="normalized cone sensitivity")+
-xlim(400,680)+
 theme(
     axis.text = element_text(color="black",size = 12),
     axis.title=element_text(face="bold", size=14),
@@ -139,7 +137,7 @@ ggsave("cones.png")
 with(spectrum.df, {
  	if( rgl.cur() == 0 ) {
     rgl.open()
-    par3d(zoom=0.8227028, userMatrix=matrix(c(0.972281754016876,0.199476152658463,0.121972970664501,0,0,0.521670699119568,-0.853146910667419,0,-0.23381219804287,0.829499185085297,0.507210910320282,0,-0.0236389161640957,0.175143327954142,2.32254899756867e-08,1),4,4),windowRect = c( 1440, 45, 3440, 1441 ), antialias=4 )
+    par3d(zoom= 0.7206817, userMatrix=matrix(c(0.965923726558685,0.205491006374359,0.157368451356888,0,0,0.608006477355957,-0.79393208026886,0,-0.258826941251755,0.766877830028534,0.587287902832031,0,0.0923089283171894,0.189200585444115,6.80704925759557e-09,1),4,4),windowRect = c( 1581, 44, 3581, 1440 ) )
     rgl.bg(color = "black" )
   }
   rgl.clear(type = c("shapes", "bboxdeco"))
@@ -161,7 +159,7 @@ with(spectrum.df, {
     rgl.texts(rbind(c(1,0.5,0)), text = c("M"), color = "white",
              adj = c(-5, 0), cex = axes.title.size)        
     rgl.points(L,M,S,col=rgb(spectrum.df %>% select(R,G,B)),size=6) 
-    text3d(L,M,S,wl.labels,adj=c(0,0),depth_test="always", cex=1.5)
+    text3d(L,M,S,wl.labels,adj=c(0,0),depth_test="always", cex=2)
     
     g.lms.df<-spectrum.df %>% select(L,M,S)
     lmsvolume<-convhulln(g.lms.df)
@@ -177,7 +175,7 @@ rgl.snapshot("LMS.png")
 with(spectrum.df, {
 	if( rgl.cur() == 0 ) {
     rgl.open()
-   par3d(zoom=0.8227028, userMatrix=matrix(c(0.972281754016876,0.199476152658463,0.121972970664501,0,0,0.521670699119568,-0.853146910667419,0,-0.23381219804287,0.829499185085297,0.507210910320282,0,-0.0236389161640957,0.175143327954142,2.32254899756867e-08,1),4,4),windowRect = c( 1440, 45, 3440, 1441 ), antialias=4)
+     par3d(zoom=0.7206817, userMatrix=matrix(c(0.965923726558685,0.205491006374359,0.157368451356888,0,0,0.608006477355957,-0.79393208026886,0,-0.258826941251755,0.766877830028534,0.587287902832031,0,0.0923089283171894,0.189200585444115,6.80704925759557e-09,1),4,4),windowRect = c( 1581, 44, 3581, 1440 ) )
     rgl.bg(color = rgb(0,0,0) )
   }
   rgl.clear(type = c("shapes", "bboxdeco"))
@@ -196,7 +194,7 @@ with(spectrum.df, {
   rgl.texts(rbind(c(1,0.5,0)), text = c("Y"), color = "white",
              adj = c(-10, 0), cex = axes.title.size)        
   rgl.points(X,Y,Z,col=rgb(spectrum.df %>% select(R,G,B)),size=6,depth_test="always") 
-  text3d(X,Y,Z,wl.labels,adj=c(0,0),depth_test="always",cex=1.5)
+  text3d(X,Y,Z,wl.labels,adj=c(0,0),depth_test="always",cex=2)
     
   g.xyz.df<-spectrum.df %>% select(X,Y,Z)
   xyzvolume<-convhulln(g.xyz.df)
@@ -224,14 +222,40 @@ sRGB.df
 #INSTRUMENTATION "D50, 2 degree, geometry 45/0, no polarisation filter, white backing, according to ISO 13655"
 #C       M     Y       K     X      Y       Z
 #100     0     0       0    15.02   22.93   52.85
+#100     100   0       0     5.67    4.10   15.67
 #0       100   0       0    33.03   16.79   15.01
+#0       100   100     0    30.20   16.02    2.30
 #0       0     100     0    69.17   74.16    7.04
-
-fcmyk.xy.df<-data.frame(x=c(15.02,33.03,69.17),y=c(22.93,16.79,74.16),z=c(52.85,15.01,7.04))
-fcmyk.xy.df<-data.frame(t(apply(fcmyk.xy.df,1,function(x){x/sum(x)})))
-fcmyk.xy.df<-fcmyk.xy.df %>% mutate (R=c(0,1,1),G=c(1,0,1),B=c(1,1,0))
+#100     0     100     0     8.16   18.42    6.74
 
 
+xy2XYZ<-function(x,y,Y=1) {
+	X<-x*Y/y
+	Z<-(1-x-y)*Y/y
+}
+
+XYZ2xy<-function(X,Y,Z) {
+    denom<-X+Y+Z
+	x<-X/denom
+	y<-Y/denom
+	c(x,y)
+}
+
+ink.XYZ.scale<-20
+
+ink.df<-data.frame(X=c(15.02,5.67,33.03,30.20,69.17,8.16)/ink.XYZ.scale,
+                   Y=c(22.93,4.10,16.79,16.02,74.16,18.42)/ink.XYZ.scale,
+                   Z=c(52.85,15.67,15.01,2.30,7.04,6.74)/ink.XYZ.scale)
+
+inks<-as.list(data.frame(apply(ink.df,1,c)))                  
+ink.rgb.df<-data.frame(do.call('rbind',lapply(inks,function(x){pmin(1,pmax(0,xyzrgbt %*% x))})))                  
+colnames(ink.rgb.df)<-c('R','G','B')
+ink.xy.df<-data.frame(do.call("rbind",lapply(inks, function(x){do.call("XYZ2xy",as.list(x))})))
+colnames(ink.xy.df)<-c('x','y')
+ink.xyz.df<-(t(apply(ink.df,1,function(x){x/sum(x)})))
+colnames(ink.xyz.df)=c('x','y','z')
+ink.df<-cbind(ink.df,ink.xy.df,ink.rgb.df)
+ink.df
 
 
 #Pigments
@@ -288,19 +312,23 @@ pigment.xy.df<-data.frame(t(apply(pigment.xyz.df,1,function(x){x/sum(x)})))
 pigment.xy.df<-pigment.xy.df %>% select(x,y)
 pigment.xy.df<-pigment.xy.df %>% mutate (R=c(0,1,1),G=c(0,0,1),B=c(1,0,0))
 
+label.size<-4.7
+dot.size<-4.5
+
 ggplot(g.xy.df,aes(x,y,fill="white"))+
-geom_point(aes(color=rgb(R,G,B)))+
 geom_polygon(color="grey90",alpha=0.5)+
 scale_color_identity()+
 scale_fill_identity()+
-geom_point(data=sRGB.df,aes(x,y,color=rgb(R,G,B)), size=8)+
 geom_polygon(data=sRGB.df,color="grey70",alpha=0.5)+
-geom_point(data=fcmyk.xy.df,aes(x,y,color=rgb(R,G,B)), size=8)+
-geom_polygon(data=fcmyk.xy.df,color="grey50",alpha=0.5)+
-geom_point(data=pigment.xy.df,aes(x,y,color=rgb(R,G,B)), size=8)+
-geom_polygon(data= pigment.xy.df,color="grey30",alpha=0.5)+
-geom_text(data=g.xy.df %>% filter(wavelength >= 525), aes(x,y,color=rgb(R,G,B),label=wl.labels),hjust=-0.5, vjust=0, cex=5)+
-geom_text(data=g.xy.df %>% filter(!(wavelength >= 525)), aes(x,y,color=rgb(R,G,B),label=wl.labels),hjust=1, vjust=0, cex=5)+
+geom_point(data=sRGB.df,aes(x,y,color=rgb(R,G,B)), size=dot.size)+
+geom_polygon(data=ink.df,color="grey50",alpha=0.5)+
+geom_point(data=ink.df,aes(x,y,color=rgb(R,G,B)), size=dot.size)+
+#geom_polygon(data= pigment.xy.df,color="grey30",alpha=0.5)+
+#geom_point(data=pigment.xy.df,aes(x,y,color=rgb(R,G,B)), size=dot.size)+
+geom_text(data=g.xy.df %>% filter(wavelength >= 525), aes(x,y,color=rgb(R,G,B),label=wl.labels),hjust=-0.5, vjust=0, cex=label.size)+
+geom_text(data=g.xy.df %>% filter(!(wavelength >= 525),wavelength>450), aes(x,y,color=rgb(R,G,B),label=wl.labels),hjust=1.3, vjust=0, cex=label.size)+
+xlim(c(0,0.7))+
+geom_point(aes(color=rgb(R,G,B)),size=dot.size)+
 theme(
     axis.text = element_text(color="black",size = 12),
     axis.title=element_text(face="bold", size=14),
