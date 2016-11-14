4b64e09692e7baf2ba4b9b3cab52b843c59f1d5b
GIT Admin <maneesh.yadav@sri.com>
2016-11-13 23:34:41 -0800

    added LMS triangle, small fixes

--- graphs_all.R
+++ graphs_all.R
@@ -2,7 +2,6 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(colorscience)
-library(ggspectra)
 library(scatterplot3d)
 library(rgl)
 library(geometry)
@@ -10,11 +9,12 @@ library(geometry)
 #careful using datasets from colorscience packages, some are log some are linear and they aren't labelled in a way that makes it easy to tell...they should all be linear
 o.df<-StockmanSharpe2degCMFadj2000
 o.df
+o.df[is.na(o.df)] <- 0
 
 colnames(o.df)=c("wavelength","L","M","S")
 
 purple1.wl<-400
-purple2.wl<-615
+purple2.wl<-680
 lms.df <- o.df %>% filter(wavelength>=purple1.wl,wavelength<=purple2.wl )
 lms.df
 
@@ -22,10 +22,11 @@ lms.purple1<-lms.df %>% filter(wavelength==purple1.wl)
 lms.purple2<-lms.df %>% filter(wavelength==purple2.wl)
 
 purple.funcs<-apply(rbind(lms.purple1,lms.purple2), 2, approxfun, x=c(purple1.wl, purple2.wl))
-purples.lms.df<-data.frame(t(sapply(seq(purple1.wl,purple2.wl,25),function(x){c(NA,purple.funcs$L(x),purple.funcs$M(x),purple.funcs$S(x))})))
+purples.lms.df<-data.frame(t(sapply(seq(purple1.wl,purple2.wl,30),function(x){c(NA,purple.funcs$L(x),purple.funcs$M(x),purple.funcs$S(x))})))
 colnames(purples.lms.df)=colnames(lms.df)
+lms.df<-rbind(lms.df,purples.lms.df %>% arrange(-row_number()))
 
-lms.df<-rbind(lms.df,purples.lms.df)
+lms.df
 
 lmss<-as.list(data.frame(apply(lms.df[,c("L","M","S")],1,c)))
 names(lmss)<-lms.df$wavelength
@@ -58,12 +59,9 @@ xyzrgbt
 lms2xyz<-function(x){lmst %*% x}
                               
 lms2rgbtt<-function(x){((xyzrgbt %*% lmst) %*% x)}
-
 xyz.df<-data.frame(do.call("rbind",lapply(lmss,function(x){t(lms2xyz(x))})))
 colnames(xyz.df)<-c('X','Y','Z')
 
-
-#rgb.df<-do.call("rbind",lapply(lmss,function(x){pmin(1,pmax(0,lms2rgbtt(x)))}))
 rgb.df<-do.call("rbind",lapply(lmss,function(x){pmin(1,pmax(0,sapply(lms2rgbtt(x),gammacor)))}))
 colnames(rgb.df)<-c('R','G','B')
 
@@ -125,9 +123,11 @@ theme(
     axis.text = element_text(color="black",size = 12),
     axis.title=element_text(face="bold", size=14),
     panel.grid.major = element_line(colour = "grey50"),
-    panel.grid.minor = element_blank(),
+    panel.grid.minor = element_line(colour = "grey40"),
     panel.background = element_rect(fill = "black")
-  )
+  )+
+scale_x_continuous(minor_breaks=seq(400,680,25), breaks=seq(400,680,50))
+
 
 
 ggsave("cones.png")
@@ -137,13 +137,13 @@ ggsave("cones.png")
 with(spectrum.df, {
  	if( rgl.cur() == 0 ) {
     rgl.open()
-    par3d(zoom= 0.7206817, userMatrix=matrix(c(0.965923726558685,0.205491006374359,0.157368451356888,0,0,0.608006477355957,-0.79393208026886,0,-0.258826941251755,0.766877830028534,0.587287902832031,0,0.0923089283171894,0.189200585444115,6.80704925759557e-09,1),4,4),windowRect = c( 1581, 44, 3581, 1440 ) )
+    par3d(zoom= 0.6536796, userMatrix=matrix(c(0.972121298313141,0.171159878373146,0.160263866186142,0,0,0.683490395545959,-0.729959487915039,0,-0.23447859287262,0.709609150886536,0.664435565471649,0,0.192842816262267,0.20928195738179,4.70598084945095e-08,1),4,4),windowRect = c( 1748, 44, 3748, 1440 ) )
     rgl.bg(color = "black" )
   }
   rgl.clear(type = c("shapes", "bboxdeco"))
  # rgl.viewpoint(theta = 0, phi = -130, zoom = 1)
   axes.title.size=2
-  axes3d( edges=c("x--", "y+-", "z--"), col="white", cex=1.5,nticks=5)
+  axes3d( edges=c("x--", "y+-", "z--"), col="white", cex=2,nticks=5)
 	
 	box.color=rgb(0.6,0.6,0.6)
 	box.alpha=0.5
@@ -158,8 +158,8 @@ with(spectrum.df, {
              adj = c(8, 8), cex = axes.title.size)
     rgl.texts(rbind(c(1,0.5,0)), text = c("M"), color = "white",
              adj = c(-5, 0), cex = axes.title.size)        
-    rgl.points(L,M,S,col=rgb(spectrum.df %>% select(R,G,B)),size=6) 
-    text3d(L,M,S,wl.labels,adj=c(0,0),depth_test="always", cex=2)
+    rgl.points(L,M,S,col=rgb(spectrum.df %>% select(R,G,B)),size=17) 
+    text3d(L,M,S,wl.labels,adj=c(-0.3,0),depth_test="always", cex=2)
     
     g.lms.df<-spectrum.df %>% select(L,M,S)
     lmsvolume<-convhulln(g.lms.df)
@@ -169,14 +169,75 @@ with(spectrum.df, {
 rgl.snapshot("LMS.png")
 
 
+#For 2D LMS mixing triangle
+euler.angles.matrix<-function(phi,theta,psi) {
+  D<-t(matrix(c(          cos(phi),      sin(phi),          0,
+                         -sin(phi),      cos(phi),          0,
+                                 0,              0,         1),3,3))
+
+  C<-t(matrix(c(                 1,             0,           0,
+                                 0,    cos(theta),  sin(theta),
+                                 0,   -sin(theta),  cos(theta)),3,3))
+
+  B<-t(matrix(c(          cos(psi),  sin(psi),   0,
+                         -sin(psi),  cos(psi),   0,
+                                 0,         0,   1),3,3))
+                                 
+  D %*% C %*% B                
+}
+
+
+eangles=c(pi/4,0,0)
+basis2d.df<-data.frame(t(apply(matrix(c(1,0,0,
+                                        0,1,0,
+                                        0,0,1),3,3),
+                                        1,function(x){do.call("euler.angles.matrix",as.list(eangles))%*%x})))
+colnames(basis2d.df)<-c('L2d','M2d','S2d')
+basis2d.df$type=c('L','M','S')
+basis2d.df
+
+lms2d.df<-data.frame(t(apply(lmsnorm.df,1,function(x){do.call("euler.angles.matrix",as.list(eangles))%*%x})))
+colnames(lms2d.df)<-c('L2d','M2d','S2d')
+lms2d.df
+
+
+lms2rgbttYadj<-function(x){ret<-(lmst %*% x) *c(1,0.5,1)
+	                         xyzrgbt %*% ret}
+
+
+rgb2d.df<-do.call("rbind",lapply(lmss,function(x){pmin(1,pmax(0,sapply(lms2rgbttYadj(x),gammacor)))}))
+colnames(rgb2d.df)<-c('R','G','B')
+rgb2d.df
+
+spectrum2d.df<-cbind(lms2d.df,rgb2d.df)
+
+ggplot(spectrum2d.df,aes(x=M2d,y=S2d))+
+geom_polygon(data=basis2d.df,color="grey50",fill="black",alpha=1)+
+geom_point(data=basis2d.df, aes(color="white",size=4))+
+geom_text(data=basis2d.df, aes(label=type)) +
+geom_polygon(color="grey60",fill="grey60")+
+geom_point(aes(color=rgb(R,G,B),size=4))+
+scale_color_identity()+
+theme(
+#    axis.text = element_text(color="black",size = 12),
+#    axis.title=element_text(face="bold", size=14),
+    axis.title=element_blank(),
+    axis.text=element_blank(),
+    axis.ticks=element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_rect(fill = "black"),
+    legend.position="none"
+  )
+ggsave("LMStriangle.png")
 
 
 #run this 'with' block for the 3D XYZ chart
 with(spectrum.df, {
-	if( rgl.cur() == 0 ) {
+ 	if( rgl.cur() == 0 ) {
     rgl.open()
-     par3d(zoom=0.7206817, userMatrix=matrix(c(0.965923726558685,0.205491006374359,0.157368451356888,0,0,0.608006477355957,-0.79393208026886,0,-0.258826941251755,0.766877830028534,0.587287902832031,0,0.0923089283171894,0.189200585444115,6.80704925759557e-09,1),4,4),windowRect = c( 1581, 44, 3581, 1440 ) )
-    rgl.bg(color = rgb(0,0,0) )
+    par3d(zoom= 0.6479588, userMatrix=matrix(c(0.972121298313141,0.171159878373146,0.160263866186142,0,0,0.683490395545959,-0.729959487915039,0,-0.23447859287262,0.709609150886536,0.664435565471649,0,0.223878460776204,0.14721071554287,2.24848451108039e-08,1),4,4),windowRect = c( 2455, 44, 4455, 1440 ) )
+    rgl.bg(color = "black" )
   }
   rgl.clear(type = c("shapes", "bboxdeco"))
  	
@@ -193,13 +254,13 @@ with(spectrum.df, {
              adj = c(8, 8), cex = axes.title.size)
   rgl.texts(rbind(c(1,0.5,0)), text = c("Y"), color = "white",
              adj = c(-10, 0), cex = axes.title.size)        
-  rgl.points(X,Y,Z,col=rgb(spectrum.df %>% select(R,G,B)),size=6,depth_test="always") 
-  text3d(X,Y,Z,wl.labels,adj=c(0,0),depth_test="always",cex=2)
+  rgl.points(X,Y,Z,col=rgb(spectrum.df %>% select(R,G,B)),size=17,depth_test="always") 
+  text3d(X,Y,Z,wl.labels,adj=c(0,-0.5),depth_test="always",cex=2)
     
   g.xyz.df<-spectrum.df %>% select(X,Y,Z)
   xyzvolume<-convhulln(g.xyz.df)
   triangles3d(g.xyz.df[t(xyzvolume), ], color = rgb(1,1,1), depth_test="always",alpha = 0.4)
-  axes3d( edges=c("x--", "y+-", "z--"), col="white", nticks=5,cex=1.5)
+  axes3d( edges=c("x--", "y+-", "z--"), col="white", nticks=5,cex=2)
 	
 })
 
@@ -210,7 +271,7 @@ rgl.snapshot("XYZ.png")
 
 #run this for the CIE xyY chromaticity diagram with embdedded gamuts
 
-g.xy.df<-spectrum.df %>% select(x,y,Y,R,G,B,wavelength,wl.labels)
+g.xy.df<-spectrum.df %>% select(x,y,Y,R,G,B,wl.labels)
 g.xy.df
 
 sRGB.df<-data.frame(x=c(0.64,0.3000,0.1500),y=c(0.3300,0.6000,0.0600),R=c(1,0,0),G=c(0,1,0),B=c(0,0,1))
@@ -267,12 +328,15 @@ ink.df
 
 #https://en.wikipedia.org/wiki/Illuminant_D65
 xy.refwhite<-c(0.31271,0.32902)
-XYZ.refwhite<-c(95.047,100.00,108.883)/100
+XYZ.refwhite<-c(95.047,100.00,108.883)
 XYZ.refwhite.list<-as.list(XYZ.refwhite)
 
-yellowochre.Lab<-c(61.2,15.03,43.46)
-cadmiumred.Lab<-c(38.2,42.47,19.41)
-ultramarineblue.Lab<-c(24.36,12.28,-25.74)
+
+https://people.csail.mit.edu/jaffer/Color/winsor-newton-lab.txt
+
+yellowochre.Lab<-c(67.01,15.24,76.01)
+cadmiumred.Lab<-c(33.1,43.43,36.29)
+ultramarineblue.Lab<-c(30.85,17.31,-72.01)
 
 
 Lab2XYZ<-function(L,a,b,Xw,Yw,Zw){
@@ -308,7 +372,10 @@ pigment.xy.df<-data.frame(do.call("rbind",lapply(list(ub=ultramarineblue.Lab,cr=
 pigment.xy.df
 
 colnames(pigment.xy.df)<-c('x','y','z')
-pigment.xy.df<-data.frame(t(apply(pigment.xyz.df,1,function(x){x/sum(x)})))
+pigment.xy.df<-data.frame(t(apply(pigment.xy.df,1,function(x){x/sum(x)})))
+pigment.xy.df
+
+
 pigment.xy.df<-pigment.xy.df %>% select(x,y)
 pigment.xy.df<-pigment.xy.df %>% mutate (R=c(0,1,1),G=c(0,0,1),B=c(1,0,0))
 
@@ -325,9 +392,8 @@ geom_polygon(data=ink.df,color="grey50",alpha=0.5)+
 geom_point(data=ink.df,aes(x,y,color=rgb(R,G,B)), size=dot.size)+
 #geom_polygon(data= pigment.xy.df,color="grey30",alpha=0.5)+
 #geom_point(data=pigment.xy.df,aes(x,y,color=rgb(R,G,B)), size=dot.size)+
-geom_text(data=g.xy.df %>% filter(wavelength >= 525), aes(x,y,color=rgb(R,G,B),label=wl.labels),hjust=-0.5, vjust=0, cex=label.size)+
-geom_text(data=g.xy.df %>% filter(!(wavelength >= 525),wavelength>450), aes(x,y,color=rgb(R,G,B),label=wl.labels),hjust=1.3, vjust=0, cex=label.size)+
-xlim(c(0,0.7))+
+geom_text(data=g.xy.df %>% filter(wl.labels >= 525), aes(x,y,color=rgb(R,G,B),label=wl.labels),hjust=-0.5, vjust=0, cex=label.size)+
+geom_text(data=g.xy.df %>% filter(!(wl.labels >= 525), wl.labels>450), aes(x,y,color=rgb(R,G,B),label=wl.labels),hjust=1.3, vjust=0, cex=label.size)+
 geom_point(aes(color=rgb(R,G,B)),size=dot.size)+
 theme(
     axis.text = element_text(color="black",size = 12),
@@ -336,6 +402,7 @@ theme(
     panel.grid.minor = element_blank(),
     panel.background = element_rect(fill = "black")
   )
+  
 
 ggsave("xyY.png")
 
